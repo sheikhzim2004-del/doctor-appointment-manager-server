f3bfa5a056557699882aec34edb2a2f6d0da68f6
sheikhzim2004-del <sheikhzim2004@gmail.com>
2026-06-06 01:47:07 +0600

    creat doctor appointment manager server and create post api

---
+++ index.js
@@ -0,0 +1,62 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+dotenv.config();
+
+const uri = process.env.MONGODB_URI;
+
+
+const app = express();
+const PORT = process.env.PORT;
+app.use(cors())
+app.use(express.json())
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+async function run() {
+    try {
+        await client.connect();
+        
+        const db = client.db("docappoints")
+        const appointCollection = db.collection("appoints")
+
+        app.post("/appoint", async(req, res)=> {
+            const appointData = req.body
+            console.log(appointData);
+            const result = await appointCollection.insertOne(appointData)
+
+            res.send(result)
+        })
+
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send("Server is running Perfectly")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
