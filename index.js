e1efa6692e93dd38798726fa066ac1720ca43aca
sheikhzim2004-del <sheikhzim2004@gmail.com>
2026-08-07 01:46:17 +0600

    veryfy JWT token and secure

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const cors = require('cors')
 dotenv.config();
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 const uri = process.env.MONGODB_URI;
 const PORT = process.env.PORT;
 
@@ -26,6 +27,33 @@ const client = new MongoClient(uri, {
     }
 });
 
+const JWKS = createRemoteJWKSet(
+    new URL("http://localhost:3000/api/auth/jwks")
+)
+
+//midleware
+const verifyToken = async(req, res, next) => {
+    const authHeader = req.headers.authorization
+    if(!authHeader){
+        return res.status(401).json({message: "Unauthorized"});
+    }
+
+    const token = authHeader.split(" ")[1];
+    if(!token){
+        return res.status(401).json({message: "Unauthorized"});
+    }
+    console.log(token)
+
+    try{
+        const {payload} = await jwtVerify(token, JWKS)
+        console.log('payload', payload)
+        next()
+    } catch(error){
+        return res.status(403).json({message: "Forbidden"})
+    }
+
+}
+
 
 async function run() {
     try {
@@ -40,25 +68,26 @@ async function run() {
             res.json(result);
         });
 
-        app.get("/doctors/:id", async (req, res) => {
+        
+        app.get("/doctors/:id", verifyToken, async (req, res) => {
             const {id} = req.params;
             const doctorId = new ObjectId(id);
             const result = await doctorsCollection.findOne({_id: doctorId})
             res.send(result)
         })
 
-         app.get("/appoint", async (req, res) => {
+         app.get("/appoint", verifyToken, async (req, res) => {
             const result = await appointCollection.find().toArray();
             res.send(result)
         })
 
-        app.post("/appoint", async(req, res)=> {
+        app.post("/appoint", verifyToken, async(req, res)=> {
             const appointData = req.body;
             console.log('appointment data', appointData);
             const result = await appointCollection.insertOne(appointData)
             res.send(result)
         })
-        app.patch("/appoint/:id", async(req, res)=> {
+        app.patch("/appoint/:id", verifyToken, async(req, res)=> {
             const {id} = req.params;
             const updatedAppoint = req.body;
 
@@ -68,7 +97,7 @@ async function run() {
             )
             res.send(result)
         })
-        app.delete("/appoint/:id", async(req, res) =>{
+        app.delete("/appoint/:id", verifyToken,  async(req, res) =>{
             const {id} = req.params;
             const result = await appointCollection.deleteOne({_id: new ObjectId(id)})
             res.send(result)
